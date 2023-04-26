ff149e87261f5de52ac04b534fcb1ce4fb2d2217
kamal-hutech <kamal@hutechsolutions.com>
2023-04-26 16:20:47 +0530

    Folder Structure and Redux Added

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react"
+import { fetchDataFromApi } from "./utils/api"
+import { useSelector, useDispatch } from "react-redux"
+
+import { getApiConfiguration, getGenres } from "./store/homeSlice"
+
+function App() {
+  const dispatch = useDispatch()
+  const { url } = useSelector((state) => state.home)
+  useEffect(() => {
+    apiTesting()
+  })
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      dispatch(getApiConfiguration(res))
+    })
+  }
+  return <div className="App">{url?.total_pages}</div>
+}
+
+export default App
