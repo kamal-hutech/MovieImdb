0be3b6fd2bc5ca4cbfee9b3acd377b574e7f4766
kamal-hutech <kamal@hutechsolutions.com>
2023-04-26 16:37:25 +0530

    Routes Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react"
-import { fetchDataFromApi } from "./utils/api"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux"
-import { getApiConfiguration, getGenres } from "./store/homeSlice"
 
+import { getApiConfiguration, getGenres } from "./store/homeSlice"
+import { fetchDataFromApi } from "./utils/api"
 import Header from "./components/header/Header"
 import Footer from "./components/footer/Footer"
 import Home from "./pages/home/Home"
@@ -22,7 +23,19 @@ function App() {
       dispatch(getApiConfiguration(res))
     })
   }
-  return <div className="App">{url?.total_pages}</div>
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
 }
 
 export default App
