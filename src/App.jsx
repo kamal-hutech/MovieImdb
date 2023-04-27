125cad9661831f1faae2ae1d1d5a15acc7db98e3
kamal-hutech <kamal@hutechsolutions.com>
2023-04-27 11:47:13 +0530

    HeroBanner Complete

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,16 @@ function App() {
   const dispatch = useDispatch()
   const { url } = useSelector((state) => state.home)
   useEffect(() => {
-    apiTesting()
+    fetchApiConfig()
   })
-  const apiTesting = () => {
-    fetchDataFromApi("/movie/popular").then((res) => {
-      dispatch(getApiConfiguration(res))
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      }
+      dispatch(getApiConfiguration(url))
     })
   }
   return (
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,7 +5,11 @@ import HeroBanner from "./heroBanner/HeroBanner"
 import "./style.scss"
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div>
+      <HeroBanner />
+    </div>
+  )
 }
 
 export default Home
